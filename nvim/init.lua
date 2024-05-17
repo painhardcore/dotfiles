5052fd0ea5249c3f102ec9d5f3e697412c7f57f0
Andrey Yurchenkov <painhardcore@gmail.com>
2024-05-17 03:00:56 +0200

    change colorscheme

--- nvim/init.lua
+++ nvim/init.lua
@@ -669,7 +669,7 @@ require("lazy").setup({
 			-- Load the colorscheme here.
 			-- Like many other themes, this one has different styles, and you could load
 			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("catppuccin")
+			vim.cmd.colorscheme("catppuccin-frappe")
 
 			-- You can configure highlights by doing something like:
 			vim.cmd.hi("Comment gui=none")
